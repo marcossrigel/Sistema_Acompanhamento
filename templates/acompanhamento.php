721237b46b44c4e79ef8c9b235fcf37eb8fe9ac6
Marcos Rigel <marcosrigel1999@gmail.com>
2025-08-28 17:04:11 -0300

    ultimo ajuste

--- templates/acompanhamento.php
+++ templates/acompanhamento.php
@@ -1,6 +1,6 @@
 <?php
 if (session_status() !== PHP_SESSION_ACTIVE) { session_start(); }
-require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/config.php';
 
 $conn = $connLocal ?? $conn ?? $conexao ?? null;
 $conn->set_charset('utf8mb4');
